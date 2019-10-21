89b1a7e01cf26d3ae315d93c32441085a880bc2f
Drew Scheffer <drewscheffer@installs-MacBook-Air.local>
2019-10-21 09:32:18 -0400

    First Commit with Xcode Files

---
+++ Wasted/Wasted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wasted
+//
+//  Created by Drew Scheffer on 9/10/19.
+//  Copyright © 2019 Drew Scheffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
