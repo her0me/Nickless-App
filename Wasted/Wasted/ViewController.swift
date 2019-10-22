bee2f1e0c5daa761539b607281de0f444fb8f977
Drew Scheffer <drewscheffer@installs-MacBook-Air.local>
2019-10-22 11:29:07 -0400

    Added a text box that almost works

--- Wasted/Wasted/ViewController.swift
+++ Wasted/Wasted/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return priorityTypes.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return priorityTypes[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectPrioriy = priorityTypes[row]
+        textField.text = selectPrioriy
+    }
 
+    var selectPrioriy : String?
+    
+    var priorityTypes = ["low", "medium", "high"]
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    func createPickerView(){
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        textField.inputView = pickerView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createPickerView
     }
 
 
