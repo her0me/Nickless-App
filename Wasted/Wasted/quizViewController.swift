7b03c145e63dd3f8ec1e67dd686afb5048ac4d59
schefferac2020 <54146662+schefferac2020@users.noreply.github.com>
2019-10-25 08:43:07 -0400

    idek

--- Wasted/Wasted/quizViewController.swift
+++ Wasted/Wasted/quizViewController.swift
@@ -29,6 +29,59 @@ class quizViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     var priorityTypes = ["1", "2", "3", "4"]
     
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    
+    
+    @IBOutlet weak var smallBtn: UIButton!
+    @IBOutlet weak var regBtn: UIButton!
+    @IBOutlet weak var largeBtn: UIButton!
+    
+    
+    @IBAction func onClickBtn1(_ sender: Any) {
+        if btn2.isSelected{
+            btn2.isSelected = !btn2.isSelected
+        }
+        btn1.isSelected = !btn1.isSelected
+    }
+    
+    @IBAction func onClickBtn2(_ sender: Any) {
+        if btn1.isSelected{
+            btn1.isSelected = !btn1.isSelected
+        }
+        btn2.isSelected = !btn2.isSelected
+    }
+    
+    @IBAction func onClickSmalBtn(_ sender: Any) {
+        if regBtn.isSelected{
+            regBtn.isSelected = !regBtn.isSelected
+        }
+        if largeBtn.isSelected{
+            largeBtn.isSelected = !largeBtn.isSelected
+        }
+        smallBtn.isSelected = !smallBtn.isSelected
+    }
+    @IBAction func onClickRegBtn(_ sender: Any) {
+        if smallBtn.isSelected{
+            smallBtn.isSelected = !smallBtn.isSelected
+        }
+        if largeBtn.isSelected{
+            largeBtn.isSelected = !largeBtn.isSelected
+        }
+        regBtn.isSelected = !regBtn.isSelected
+    }
+    @IBAction func onClickLargeBtn(_ sender: Any) {
+        if smallBtn.isSelected{
+            smallBtn.isSelected = !smallBtn.isSelected
+        }
+        if regBtn.isSelected{
+            regBtn.isSelected = !regBtn.isSelected
+        }
+       
+        largeBtn.isSelected = !largeBtn.isSelected
+    }
+    
+    
     
     
     func createPickerView(){
